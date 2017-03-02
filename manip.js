7954cdaccb6a736987b7d7b6d899a176a869a4df
LambertTimothee <info.tim.lambert@gmail.com>
2017-03-02 10:48:51 +0100

    Debug and clean code
    -Debug html id
    -Clean code and comment (a little)

--- manip.js
+++ manip.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    //Collect all links from the jsonFile
     $.ajax({
         url: 'json.json',
         type: 'POST',
@@ -6,35 +7,35 @@ $(document).ready(function() {
         success: function(data) {
             var links = data;
             var nodes = {};
-      
-            jQuery('#nbrelations').keyup(function () { 
-              this.value = this.value.replace(/[^0-9\.]/g,'');
-            });
+
 
             // Compute the distinct nodes from the links.
             links.forEach(function(link) {
-                
+
                 link.source = nodes[link.source] || (nodes[link.source] = {
                     name: link.source,
                     value: link.sourceID
                 });
 
-                
+
                 link.target = nodes[link.target] || (nodes[link.target] = {
                     name: link.target,
                     value: link.targetID
                 });
             });
 
+            // Select distance between nodes
             dist = $('#rangedefault').val();
-            $('#rangedefault').change(function(){
-              force.linkDistance(dist)
+            $('#rangedefault').change(function() {
+                force.linkDistance(dist)
             });
 
 
+            console.log($("#nav").height())
 
-            var width = screen.width,
-                height = screen.height;
+            //Width of the window
+            var width = $(window).width(),
+                height = $(window).height();
 
             var force = d3.layout.force()
                 .nodes(d3.values(nodes))
@@ -87,12 +88,9 @@ $(document).ready(function() {
                 .enter().append("circle")
                 .attr("r", 20)
                 .call(force.drag)
-                .attr("id",function(d){
+                .attr("id", function(d) {
                     return d.value;
                 });
-
-                
-
 
 
 
@@ -123,57 +121,64 @@ $(document).ready(function() {
             function transform(d) {
                 return "translate(" + d.x + "," + d.y + ")";
             }
-            var i = 0;
 
+            //Click on nodes for relations
             $("circle").click(function() {
-                
+
                 $(this).attr('class', 'selected');
-                startRelation(links,$(this))
+                startRelation(links, $(this))
             });
 
-            $("#nodeSearchBtn").click(function(){
-              var str = $("#nodeSearchTxt").val().toLowerCase();
-              $("circle").attr("class", 'normal');
-                Object.values(nodes).forEach(function(node){
-                  if (node.name.toLowerCase().indexOf(str) >= 0)
-                  {
-                    var cir = $("#"+node.value)
-                    cir.attr("class","detected")
-                  }
+            //Search function
+            $("#btnNodeSearcher").click(function() {
+                console.log("bonjour")
+                var str = $("#txtNodeSearcher").val().toLowerCase();
+                $("circle").attr("class", 'normal');
+                Object.values(nodes).forEach(function(node) {
+                    if (node.name.toLowerCase().indexOf(str) >= 0) {
+                        var cir = $("#" + node.value)
+                        cir.attr("class", "detected")
+                    }
                 })
 
             });
 
-
+            //Number of relations textBox
             $("#nbrelations").change(function() {
                 var selected = $(".selected").attr('id');
                 var value = $(this).text();
                 if (!(value == '' && value == null && value == '0')) {
                     //loadrelatedrelation(links, selected, value);
-                  startRelation(links,null)
+                    startRelation(links, null)
                 }
             });
 
-      $(function(){
-      $('#cash2').click(function(){
-        $('#nav').slideToggle("slow");
-        if($('.fleche').hasClass("flecheDown")){
-          $(".fleche").removeClass("flecheDown");
-        }else{
-          $(".fleche").addClass("flecheDown");
-        }
-      });
-    });
+            // Hide/Show nav
+            $('#cash2').click(function() {
+                $('#nav').slideToggle("slow");
+                if ($('.fleche').hasClass("flecheDown")) {
+                    $(".fleche").removeClass("flecheDown");
+                }else {
+                    $(".fleche").addClass("flecheDown");
+                }
+            });
 
-      $("#buttonSourceTarget").click(function(){
-        if($(this).hasClass("source")){
-          $(this).attr("class","target").html("Target");
-        }
-        else{
-          $(this).attr("class","source").html("Source");
-        }
-        startRelation(links,null);
-      });
+            //Switch between Source and target selector
+            $("#buttonSourceTarget").click(function() {
+                if ($(this).hasClass("source")) {
+                    $(this).attr("class", "target").html("Target");
+                } else {
+                    $(this).attr("class", "source").html("Source");
+                }
+                startRelation(links, null);
+            });
+
+            //TextBox of number relation inspector
+            jQuery('#nbrelations').keyup(function() {
+                this.value = this.value.replace(/[^0-9\.]/g, '');
+            });
+
+
 
         }
     })
@@ -182,108 +187,105 @@ $(document).ready(function() {
 
 
 
-
-function resetNodes(links){
-  links.forEach(function(link) {
-    link.target.selected = false;
-    link.source.selected = false;
-  });
+// Unselect all nodes
+function resetNodes(links) {
+    links.forEach(function(link) {
+        link.target.selected = false;
+        link.source.selected = false;
+    });
 }
 
-
-function getSource(links, selected, value){
-  if(typeof related == 'undefined')
-    {
-      var related = [];
+//Select recursively all the source of a target.
+function getSource(links, selected, value) {
+    if (typeof related == 'undefined') {
+        var related = [];
     }
-    value --;
+    value--;
     links.forEach(function(link) {
-      var source = link.source
-      var target = link.target
+        var source = link.source
+        var target = link.target
 
-      if (target.value == selected && !(source.selected) ) {
+        if (target.value == selected && !(source.selected)) {
 
-        if (value>0)
-        {
-          var relate = getSource(links,source.value,value)
-          $.merge(related,relate)
+            if (value > 0) {
+                var relate = getSource(links, source.value, value)
+                $.merge(related, relate)
+            }
+
+            var pathSelected = source.value + "-" + target.value;
+            $("#" + pathSelected).attr('class', 'link linkselector');
+
+
+
+            var sourceselector = $("#" + source.value);
+            sourceselector.attr('class', 'source');
+            related.push(source.value);
+            source.selected = true;
+
+
         }
-
-        var pathSelected = source.value+"-"+target.value;
-        console.log($("#"+pathSelected))
-        $("#"+pathSelected).attr('class', 'link linkselector');
-
-
-
-        var sourceselector = $("#" + source.value);
-        sourceselector.attr('class', 'source');
-        related.push(source.value);
-        source.selected = true;
-
-      }
     });
 
     return related;
 
-  }
+}
 
-
-function getTarget(links, selected, value){
-  if(typeof related == 'undefined')
-  {
-    var related = [];
-  }
-  value --;
-
-  links.forEach(function(link) {
-    var source = link.source
-    var target = link.target
-
-    if (source.value == selected  && !(target.selected) ) {
-
-      if (value>0)
-      {
-        var relate = getTarget(links,target.value,value);
-        $.merge(related,relate);
-      }
-        var pathSelected = source.value+"-"+target.value;
-        $("#"+pathSelected).attr('class', 'link linkselector');
-        console.log($("#"+pathSelected))
-
-      var targetselector = $("#" + target.value);
-      targetselector.attr('class', 'target');
-      related.push(target.value);
-      target.selected = true;
+//Select recursively all the target of a source.
+function getTarget(links, selected, value) {
+    if (typeof related == 'undefined') {
+        var related = [];
     }
-  });
+    value--;
 
-  return related;
+    links.forEach(function(link) {
+        var source = link.source
+        var target = link.target
+
+        if (source.value == selected && !(target.selected)) {
+
+            if (value > 0) {
+                var relate = getTarget(links, target.value, value);
+                $.merge(related, relate);
+            }
+            var pathSelected = source.value + "-" + target.value;
+            $("#" + pathSelected).attr('class', 'link linkselector');
+            console.log($("#" + pathSelected))
+
+            var targetselector = $("#" + target.value);
+            targetselector.attr('class', 'target');
+            related.push(target.value);
+            target.selected = true;
+        }
+    });
+
+    return related;
 }
 
-function startRelation(links,cercle){
-  if ((cercle == null)){
-    var selected = $(".selected").attr("id");}
-  else{
-    var selected = cercle.attr('id');}
+//Prepare to load selections (Source or target)
+function startRelation(links, cercle) {
+    if ((cercle == null)) {
+        var selected = $(".selected").attr("id");
+    } else {
+        var selected = cercle.attr('id');
+    }
 
-  if ( $( ".linkselector" ).length ) {
- 
-    console.log("oui")
- 
-}
+    if ($(".linkselector").length) {
 
-  $('.linkselector').attr('class', 'link suit');
+        console.log("oui")
 
-  var value = $("#nbrelations").val();
-  $("circle").attr("class", 'normal');
+    }
 
-  if($("#buttonSourceTarget").hasClass("source")){
-    var allRelatedNode = getSource(links,selected,value);
-  }
-  else{
-    var allRelatedNode = getTarget(links,selected,value);
-  }
+    $('.linkselector').attr('class', 'link suit');
 
-  resetNodes(links);
-  $("#"+selected).attr('class','selected')
+    var value = $("#nbrelations").val();
+    $("circle").attr("class", 'normal');
+
+    if ($("#buttonSourceTarget").hasClass("source")) {
+        var allRelatedNode = getSource(links, selected, value);
+    } else {
+        var allRelatedNode = getTarget(links, selected, value);
+    }
+
+    resetNodes(links);
+    $("#" + selected).attr('class', 'selected')
 }
