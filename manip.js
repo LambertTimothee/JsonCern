77fe390c7725be1d53f8f87b7175a726961eba11
vachon <nicolas.vachon.97@gmail.com>
2017-03-15 14:39:41 +0100

    center menu

--- manip.js
+++ manip.js
@@ -34,8 +34,8 @@ $(document).ready(function() {
             });
 
 
-            var x=$("#nav").height()
-            var y= $("#nav").width()
+            var x=$("nav").height()
+            var y= $("nav").width()
 
             //Width of the window
             var width = $(window).width(),
@@ -157,7 +157,7 @@ $(document).ready(function() {
 
             // Hide/Show nav
             $('#cash2').click(function() {
-                $("#nav").animate({width:'toggle'},350);
+                $("nav").animate({width:'toggle'},350);
                 if ($('.fleche').hasClass("flecheDown")) {
                     $(".fleche").removeClass("flecheDown");
                 }else {
