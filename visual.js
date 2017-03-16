95b3a04a05b86b002219e37571dde956784ed93d
vachon <nicolas.vachon.97@gmail.com>
2017-03-16 10:57:14 +0100

    animate menu

--- visual.js
+++ visual.js
@@ -1,18 +1,55 @@
 $(document).ready(function() {
     //TextBox of number relation inspector
-            jQuery('#nbrelations').keyup(function() {
-                this.value = this.value.replace(/[^0-9\.]/g, '');
-            });
+
+    var nav = $("nav");
+    var arrow = $('.fleche');
+
+    jQuery('#nbrelations').keyup(function() {
+        this.value = this.value.replace(/[^0-9\.]/g, '');
+    });
 
 
-                        // Hide/Show nav
-            $('#cash2').click(function() {
-                $("nav").animate({width:'toggle'},350);
-                if ($('.fleche').hasClass("flecheDown")) {
-                    $(".fleche").removeClass("flecheDown");
-                }else {
-                    $(".fleche").addClass("flecheDown");
-                }
-            });
+                // Hide/Show nav
+    $('#cash2').click(function() {
+
+        
+
+        if(arrow.hasClass("flecheRotate")){
+                nav.animate({width:'toggle'},350);
+                arrow.animate({
+                    left: "16em",
+                });
+                arrow.removeClass("flecheRotate");            
+            setTimeout(function(){
+                $("nav *").fadeIn(100);
+            }, 300);     
+        }else{
+            $("nav *").fadeOut(100);
+            setTimeout(function(){
+                nav.animate({width:'toggle'},350);
+    /*            if(arrow.hasClass("flecheRotate")){
+                    arrow.animate({left: "16em",});
+                    removeClass("flecheRotate");
+                }else{*/
+                    arrow.animate({
+                        left: "1em",
+                    });
+                    arrow.addClass("flecheRotate");            
+            }, 100);                
+        }
+
+
+
+
+
+
+/*        if ($('.fleche').hasClass("flecheRotate") && $('.fleche').hasClass("flecheLeft")) {
+            $(".fleche").removeClass("flecheLeft");
+            $(".fleche").removeClass("flecheRotate");                   
+        }else {
+            $(".fleche").addClass("flecheLeft").delay( 800 );
+            $(".fleche").addClass("flecheRotate");
+        }*/
+    });
 
 });
